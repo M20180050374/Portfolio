5b29f96b6d8db90085e16a972e4658a05de1b979
José Ferreira da Fonseca Neto <44621079+M20180050374@users.noreply.github.com>
2023-09-22 01:23:17 -0300

    Update web scrapping.py

--- anime/web scrapping.py
+++ anime/web scrapping.py
@@ -1,8 +1,4 @@
-""" Esse é um projeto pessoal que compõe meu portifólio, o projeto será divido em etapas
-    A primeira etapa consiste na busca dos dados na internet utilizando algumas bibliotecas do python
-    A escolha do site foi por facilidade, busquei o site que continha mais informações para que eu pudesse utilizar nas
-    Etapas posteriores."""
-
+[oi](www.teste)
 from bs4 import BeautifulSoup  # biblioteca que ler o html da página e permite coletar as informações
 from selenium import webdriver  # biblioteca que permite carregar o js da página (facilita clicks e preenchimentos)
 from selenium.webdriver.edge.options import Options  # importante para carregar algumas definições do navegador
